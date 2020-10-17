9f5727971991f50c6438c1e70e3dbfd37e39b67b
Gijs Nelissen <gijs@Gijss-MacBook-Pro.local>
2020-10-17 22:35:02 +0200

    Split brew and cask stuff

--- brew.sh
+++ brew.sh
@@ -1,14 +1,40 @@
-brew cask install alfred
-brew cask install firefox 
-brew cask install chrome
-brew cask install 1password
-brew cask install appcleaner
-brew cask install hammerspoon
-brew cask install bartender
-brew cask install iterm2
-brew cask install visual-studio-code
-brew cask install slack
-brew cask install viscosity
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
 brew install gh
 brew install git
-brew 
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install grep
+brew install openssh
+brew install screen
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
+
+# Install other useful binaries.
+#brew install exiv2
+brew install git
+brew install rename
+brew install ssh-copy-id
+brew install tree
+brew install htop
+
+# Remove outdated versions from the cellar.
+brew cleanup
---
+++ cask.sh
@@ -0,0 +1,34 @@
+# Software packages through cask
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install cask
+brew tap caskroom/cask
+
+# System Utils
+brew cask install bartender
+brew cask install viscosity
+brew cask install iterm2
+brew cask install hammerspoon
+
+# Used utilities
+brew cask install google-chrome
+brew cask install firefox
+brew cask install zoom
+brew cask install alfred
+brew cask install slack
+brew cask install 1password
+brew cask install vlc
+brew cask install spotify
+brew cask install appcleaner
+brew cask install whatsapp
+
+# Dev stuff
+brew cask install visual-studio-code
+brew cask install phpstorm
+brew cask install iterm2
+brew cask install paw
